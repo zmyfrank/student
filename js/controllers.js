2ee4754cbf612bf1a4d29f3eb2f90ecdff49e88e
Frank <616180981@qq.com>
2017-01-24 22:39:24 +0800

    引入了file-upload

--- js/controllers.js
+++ js/controllers.js
@@ -85,11 +85,18 @@ myAppCtrl.controller('addstudent',function ($scope,userListService) {
             wish:$scope.user.wish
         };
         userListService.addStudent(student).success(function () {
-            alert("上传成功")
+            $scope.user.name="";
+            $scope.user.type="";
+            $scope.user.school="";
+            $scope.user.wish="";
+            $scope.user.qq="";
+            $scope.user.talent=null;
+            $scope.user.level=null;
+            alert("上传成功");
         })
     };
 
-    $scope.reseatStudent = function () {
+    $scope.reseatStudent = function () {        //取消按钮
         $scope.user.name="";
         $scope.user.type="";
         $scope.user.school="";
@@ -99,3 +106,31 @@ myAppCtrl.controller('addstudent',function ($scope,userListService) {
         $scope.user.level=null;
     }
 });
+myAppCtrl.controller('info', ['$scope', '$http',,'Upload',function($scope, $http,Upload) {
+    $scope.uploadImg = '';
+    //提交
+    $scope.submit = function () {
+        $scope.upload($scope.files);
+    };
+    $scope.upload = function (file) {
+        $scope.fileInfo = file;
+        Upload.upload({
+            //服务端接收
+            url: 'Ashx/UploadFile.ashx',
+            //上传的同时带的参数
+            data: { 'username': $scope.username },
+            file: file
+        }).progress(function (evt) {
+            //进度条
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progess:' + progressPercentage + '%' + evt.config.file.name);
+        }).success(function (data, status, headers, config) {
+            //上传成功
+            console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+            $scope.uploadImg = data;
+        }).error(function (data, status, headers, config) {
+            //上传失败
+            console.log('error status: ' + status);
+        });
+    };
+}]);
